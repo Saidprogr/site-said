fe21f8210d01b5ccb476a5f5e3e720892aa73ed7
Saidprogr <sverqazov@gmail.com>
2026-05-13 20:56:53 +0400

    steqw

--- web-site seminar/script.js
+++ web-site seminar/script.js
@@ -1,7 +1,6 @@
-
 window.addEventListener('DOMContentLoaded', event => {
 
-
+    // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
@@ -15,11 +14,13 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-
+    // Shrink the navbar 
     navbarShrink();
 
+    // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
+    //  Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
@@ -28,7 +29,7 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     };
 
-
+    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
